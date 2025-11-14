b415e24b1fe0c35f26c32f807b4e7a4b76884924
Username4D <studio@kremzow.org>
2025-11-14 23:29:13 +0100

    improved shooting chances/cooldown

--- scripts/test_scene.gd
+++ scripts/test_scene.gd
@@ -4,7 +4,14 @@ var shoot_start = Vector2.ZERO
 var shoot_distance
 var mesh_start = Vector2.ZERO
 
-var planet = preload("res://scenes/planets/mercury.tscn")
+var planet_textures = [preload("res://assets/planets/mercury.svg"), preload("res://assets/planets/venus.svg"), preload("res://assets/planets/earth.svg"), preload("res://assets/planets/mars.svg")]
+var planet_scales = [Vector2(0.4,0.4),Vector2(0.6,0.6),Vector2(0.8,0.8),Vector2(1.1,1.1)]
+
+var planets = [preload("res://scenes/planets/mercury.tscn"), preload("res://scenes/planets/venus.tscn"), preload("res://scenes/planets/earth.tscn"), preload("res://scenes/planets/mars.tscn")]
+
+var next_planet = 0
+
+
 
 func _process(delta: float) -> void:
 	if not $Button.button_pressed and not $Button/Button2.button_pressed:
@@ -16,15 +23,31 @@ func _process(delta: float) -> void:
 		shoot_distance = get_local_mouse_position() - shoot_start
 		$Button/MeshInstance2D.global_position = mesh_start + (shoot_distance / 2)
 		print(shoot_distance)
-
+	
+	$Button/MeshInstance2D.scale = $Button/MeshInstance2D.scale.move_toward(planet_scales[next_planet] * 0.5, 1 * delta)
 func _on_button_button_up() -> void:
-	var new_shot = planet.instantiate()
-	new_shot.index = gameplay_handler.get_next_index()
-	new_shot.position = $Button/MeshInstance2D.global_position
-	new_shot.scale = new_shot.nscale
-	self.add_child(new_shot)
-	new_shot.apply_impulse(shoot_distance * -1)
-	print(shoot_distance)
+	if $Button/MeshInstance2D.scale == planet_scales[next_planet] * 0.5:
+		var new_shot = planets[next_planet].instantiate()
+		new_shot.index = gameplay_handler.get_next_index()
+		new_shot.rotation = $Button/MeshInstance2D.global_rotation
+		new_shot.position = $Button/MeshInstance2D.global_position
+		new_shot.scale = new_shot.nscale
+		self.add_child(new_shot)
+		new_shot.apply_impulse(shoot_distance * -1)
+		print(shoot_distance)
+		var ran = randi_range(0, 12)
+		if ran <= 5:
+			next_planet = 0
+		elif ran <= 9:
+			next_planet = 1
+		elif next_planet <= 10:
+			next_planet = 2
+		else:
+			next_planet = 3
+		$Button/MeshInstance2D.texture = planet_textures[next_planet]
+		$Button/MeshInstance2D.scale = Vector2(0.01,0.01)
+	
+	
 func _on_button_button_down() -> void:
 	mesh_start = $Button/MeshInstance2D.global_position
 	shoot_start = get_local_mouse_position()
