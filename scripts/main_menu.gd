bf7509dda2c5bf122167fb16b8a9d503234f4fe6
Username4D <studio@kremzow.org>
2025-11-16 18:04:20 +0100

    small fix for main menu

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,27 +1,34 @@
 extends Node2D
 
+var settings = false
+var play = false
+var achievements = false
+
 func _on_button_row_pressed(button: Variant) -> void:
 	match button:
 		'PLAY':
-			if self.get_node("gameplay_scene") == null:
+			if not play:
 				var new = load("res://scenes/gameplay_scene.tscn").instantiate()
 				new.position.y = - 648
 				self.get_parent().add_child(new)
+				play = true
 			$Camera2D.position.y = -648+ 648 / 2
 			$Camera2D.position.x = 1152 / 2
 		'ACHIEVEMENTS':
-			if self.get_node("achievements_page") == null:
+			if not achievements:
 				var new = load("res://scenes/achievements_page.tscn").instantiate()
 				new.position.x = 1152
 				self.get_parent().add_child(new)
+				achievements = true
 			$Camera2D.position.x = 1152 + 1152 / 2
 			$Camera2D.position.y = 648 / 2
 		'SETTINGS':
-			if self.get_node("settings_page") == null:
+			if not settings:
 				var new = load("res://scenes/settings.tscn").instantiate()
 				new.position.x = 576
 				new.position.y = 648
 				self.get_parent().add_child(new)
+				settings = true
 			$Camera2D.position.x = 576 + 1152 / 2
 			$Camera2D.position.y = 648 + 648 / 2
 func home():
